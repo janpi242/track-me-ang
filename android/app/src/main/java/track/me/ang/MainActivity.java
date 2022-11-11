fde366cdc2205eddab6f3b30a74e60e0b1beb752
Piotr Janas <piotr@code-q.pl>
2022-11-11 22:36:33 +0100

    add checking for geolocation enabled

---
+++ android/app/src/main/java/track/me/ang/MainActivity.java
@@ -0,0 +1,5 @@
+package track.me.ang;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core'
+import { Capacitor } from '@capacitor/core'
 import { Geolocation } from '@capacitor/geolocation'
+import { AlertController } from '@ionic/angular'
 import * as L from 'leaflet'
 
 @Component({
@@ -10,17 +12,62 @@ import * as L from 'leaflet'
 export class MapComponent implements AfterViewInit, OnInit {
   private map
   private zoom = 13
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
   ngOnInit(): void {
     L.Icon.Default.imagePath = 'assets/leaflet/'
   }
+
   ngAfterViewInit(): void {
     this.initMap()
+    this.checkPermissionsForGps()
+  }
+
+  async checkPermissionsForGps() {
+    const permissions = await Geolocation.checkPermissions()
+    if (
+      permissions.location === 'denied' &&
+      permissions.coarseLocation === 'denied'
+    ) {
+      const platform = Capacitor.getPlatform()
+      if (platform !== 'web') {
+        this.showInfo('inside')
+        Geolocation.requestPermissions().then(async (permission) => {
+          this.setCurrentPosition()
+        })
+      } else {
+        this.presentAlert()
+      }
+    } else {
+      this.setCurrentPosition()
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Location services not enabled.',
+      message:
+        '<p>In order to be able to track your position, geolocotaion services must be turned on on this device.</p>' +
+        '<p>Please, turn them on and rester application.</p>',
+      buttons: ['OK'],
+    })
+
+    await alert.present()
+  }
+
+  async showInfo(info: string) {
+    const alert = await this.alertController.create({
+      header: 'Info',
+      subHeader: 'System message.',
+      message: info,
+      buttons: ['OK'],
+    })
+
+    await alert.present()
   }
 
   async setCurrentPosition() {
-    // Geolocation.requestPermissions().then(async (permission) => {
     const position = await Geolocation.getCurrentPosition()
     this.map.setView(
       new L.LatLng(position.coords.latitude, position.coords.longitude),
@@ -30,12 +77,11 @@ export class MapComponent implements AfterViewInit, OnInit {
       position.coords.latitude,
       position.coords.longitude,
     ]).addTo(this.map)
-    // })
   }
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [0, 0],
+      center: [12.6125912, -7.9955131],
       zoom: this.zoom,
     })
 
@@ -56,7 +102,5 @@ export class MapComponent implements AfterViewInit, OnInit {
         this.map.invalidateSize()
       }, 1)
     })
-
-    this.setCurrentPosition()
   }
 }
