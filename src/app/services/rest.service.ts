73daa26bee1fe6a304ccd2ecd72230c78461c3a7
janpi242 <piotr@code-q.pl>
2023-12-23 18:20:58 +0100

    add request for saving location

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -16,14 +16,14 @@ export class RestService {
   constructor(private http: HttpClient) { }
 
   getToken(): Observable<ApiResult> {
-    return this.http.get<ApiResult>(`${environment.baseUrl}/sanctum/csrf-cookie`, { observe: 'response' });
+    return this.http.get<ApiResult>(`${environment.baseUrl}/sanctum/csrf-cookie`, { observe: 'response' })
   }
 
   register(registrationData): Observable<ApiResult> {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const token = document.cookie
     console.log(token);
-    const headers = new HttpHeaders(/*{ 'X-CSRF-TOKEN': ''}*/);
+    const headers = new HttpHeaders(/*{ 'X-CSRF-TOKEN': ''}*/)
     const requestOptions = {
       headers,
       withCredentials: true
@@ -42,15 +42,20 @@ export class RestService {
     )
   }
 
-  async getUserData(token): Promise<Observable<ApiResult>> {
+  async getUserData(token: string): Promise<Observable<ApiResult>> {
     const headers = new HttpHeaders({
       // eslint-disable-next-line @typescript-eslint/naming-convention
       Authorization: `Bearer ${token}`
     })
-    return this.http.get(`${environment.baseUrl}/api/user`, { headers });
+    return this.http.get(`${environment.baseUrl}/api/user`, { headers })
   }
 
   logOut(): Observable<ApiResult> {
-    return this.http.post<ApiResult>(`${environment.baseUrl}/api/logout`, {});
+    return this.http.post<ApiResult>(`${environment.baseUrl}/api/logout`, {})
+  }
+
+  saveLocation(userLocationData): Observable<ApiResult> {
+    console.log(userLocationData);
+    return this.http.post<ApiResult>(`${environment.baseUrl}/api/location`, userLocationData)
   }
 }
